04a0b4861be1dcf3f2ac51d807617793382a2f6c
ReihanFatilla <reihanfatilla@gmail.com>
2023-08-12 12:13:04 +0700

    [feat](Home): finished SearchBar and TabBar

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -5,36 +5,96 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 24, top: 32, right: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Material(
-                  elevation: 20,
-                  shadowColor: Colors.green,
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search for products...",
-                      prefixIconColor: Colors.green,
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(width: 3, color: Colors.green),
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
+    var listTabs = [
+      "smartphones",
+      "laptops",
+      "fragrances",
+      "skincare",
+      "groceries",
+      "home-decoration",
+      "furniture",
+      "tops",
+      "womens-dresses",
+      "womens-shoes",
+      "mens-shirts",
+      "mens-shoes",
+      "mens-watches",
+      "womens-watches",
+      "womens-bags",
+      "womens-jewellery",
+      "sunglasses",
+      "automotive",
+      "motorcycle",
+      "lighting"
+    ];
+
+    return DefaultTabController(
+      length: listTabs.length,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24, top: 32, right: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              buildSearchBar(),
+              SizedBox(height: 24,),
+
+              buildTabBar(listTabs),
+              SizedBox(height: 24),
+
+              buildTabBarView(listTabs)
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Expanded buildTabBarView(List<String> listTabs) {
+    return Expanded(
+                child: TabBarView(
+                    children: listTabs
+                        .map((title) => Center(
+                        child: Text('Content of Tab $title')))
+                        .toList()));
+  }
+
+  TabBar buildTabBar(List<String> listTabs) {
+    return TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              labelColor: Colors.white,
+              isScrollable: true,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.blueAccent,
+              ),
+              tabs: listTabs.map((title) => Tab(text: title)).toList(),
+            );
+  }
+
+  Column buildSearchBar() {
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(boxShadow: [
+            BoxShadow(color: Colors.blueAccent, spreadRadius: 1, blurRadius: 15)
+          ], borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          child: const TextField(
+            decoration: InputDecoration(
+              hintText: "Search for products...",
+              prefixIconColor: Colors.blueAccent,
+              fillColor: Colors.white,
+              filled: true,
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
