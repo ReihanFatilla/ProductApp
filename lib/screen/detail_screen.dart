50654865edb762f1445990536b9db5f68ecc260a
ReihanFatilla <reihanfatilla@gmail.com>
2023-08-21 16:30:21 +0700

    [feat](Detail): added detail description

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -31,17 +31,31 @@ class _DetailScreenState extends State<DetailScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _imageRow(),
-          SizedBox(height: 16,),
+          SizedBox(
+            height: 16,
+          ),
           _titleRatingRow(),
-          SizedBox(height: 16,),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            widget.product.description ?? "",
+            style:
+                getBlackTextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+          ),
+          SizedBox(
+            height: 16,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _statsRow("Price" ,widget.product.price, Icons.price_change_rounded),
+              _statsRow(
+                  "Price", widget.product.price, Icons.price_change_rounded),
               _statsRow("Stock", widget.product.stock, Icons.add_box_rounded),
-              _statsRow("Discount", widget.product.discountPercentage, Icons.discount_rounded)
+              _statsRow("Discount", widget.product.discountPercentage,
+                  Icons.discount_rounded)
             ],
-          )
+          ),
         ],
       ),
     );
@@ -49,58 +63,68 @@ class _DetailScreenState extends State<DetailScreen> {
 
   Container _statsRow(String title, num? value, IconData icon) {
     return Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(10)
+      decoration: BoxDecoration(
+          color: Colors.grey.shade200, borderRadius: BorderRadius.circular(10)),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 20,
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: getBlackTextStyle(
+                    fontSize: 11, fontWeight: FontWeight.normal),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Row(
-                children: [
-                  Icon(icon, size: 20,),
-                  SizedBox(width: 12,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(title, style: getBlackTextStyle(fontSize: 11, fontWeight: FontWeight.normal),),
-                      Text((value ?? 0).toString(), style: getBlackTextStyle(fontSize: 13, fontWeight: FontWeight.bold),),
-                    ],
-                  )
-
-                ],
+              Text(
+                (value ?? 0).toString(),
+                style: getBlackTextStyle(
+                    fontSize: 13, fontWeight: FontWeight.bold),
               ),
-            );
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   Row _titleRatingRow() {
     return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.product.category ?? "",
-                  style: getBlackTextStyle(
-                      fontSize: 12, fontWeight: FontWeight.normal),
-                ),
-                Text(
-                  widget.product.title ?? "",
-                  style: getBlackTextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                )
-              ],
+            Text(
+              widget.product.category ?? "",
+              style: getBlackTextStyle(
+                  fontSize: 12, fontWeight: FontWeight.normal),
             ),
-            RatingBarIndicator(
-              rating: (widget.product.rating ?? 0).toDouble(),
-              itemBuilder: (context, index) => Icon(
-                Icons.star,
-                color: Colors.amber,
-              ),
-              itemSize: 24.0,
+            Text(
+              widget.product.title ?? "",
+              style:
+                  getBlackTextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             )
           ],
-        );
+        ),
+        RatingBarIndicator(
+          rating: (widget.product.rating ?? 0).toDouble(),
+          itemBuilder: (context, index) => Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          itemSize: 24.0,
+        )
+      ],
+    );
   }
 
   Container _imageRow() {
@@ -128,9 +152,7 @@ class _DetailScreenState extends State<DetailScreen> {
           Expanded(
               flex: 2,
               child: Column(
-                children: (widget.product.images ?? [])
-                    .take(3)
-                    .map((image) {
+                children: (widget.product.images ?? []).take(3).map((image) {
                   return Expanded(
                     child: Container(
                       padding: EdgeInsets.symmetric(vertical: 8),
